7200a77df7dcdd574d36104cef58374c2a26e2c4
Jared Norman <jared@super.gd>
2018-11-22 16:25:17 -0800

    Add simplest possible method

--- lib/evil_ipsum.rb
+++ lib/evil_ipsum.rb
@@ -2,4 +2,9 @@ require "evil_ipsum/version"
 require "evil_ipsum/words"
 
 module EvilIpsum
+  class << self
+    def word
+      WORDS.sample
+    end
+  end
 end
--- test/evil_ipsum_test.rb
+++ test/evil_ipsum_test.rb
@@ -4,4 +4,8 @@ class EvilIpsumTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::EvilIpsum::VERSION
   end
+
+  def test_retrieving_a_word
+    assert EvilIpsum.word.match?(/\w+/)
+  end
 end
